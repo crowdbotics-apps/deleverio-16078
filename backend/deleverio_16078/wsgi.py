31ec917b2d49164af41af532a8741a1e3757e77a
Crowdbotics <team@crowdbotics.com>
2020-04-20 21:30:37 +0000

    Update models

--- backend/deleverio_16078/wsgi.py
+++ backend/deleverio_16078/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deleverio_16078.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deleverio_16078.settings")
 
 application = get_wsgi_application()
